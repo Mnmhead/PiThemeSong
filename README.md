49e4ee7ba7733d2120a6e9c9f441b7e097772f40
Gyorgy Muntean <wyatt.g.muntean@gmail.com>
2018-06-18 06:56:54 +0000

    refactored some code

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
-Have you ever wanted a themesong to play when you enter your apartment?
+Have you ever wanted a themesong to play when you enter your home?
 Now made possible by themesong.py!
 
-themesong.py is a script runnable on the raspberry pi. 
 Configure the themesong with your phone's or laptop's mac address 
 and a song of your choice. Whenever the device connects to the nextwork,
 the song will play. 
+
+To configure the program simply create a text file called 'macs.txt'.
+Inside, write a MAC address on the first line followed by a filepath to an mp3
+on the next line.  In this way, lines alternate between MAC address and filepath
+with the first line being a MAC address.
--- themesong.py
+++ themesong.py
@@ -1,77 +1,122 @@
-import re
-import subprocess
+# Copyright Gyorgy Wyatt Muntean 2018
+#
+# This program takes in a file which serves as a map of MAC addresses
+# to file paths of Mp3s.  This program then monitors the nextwork using ARP
+# to determine when it is appropriate to play each device's themesong.
+#
+# The filename is hardcoded into this program, maybe later I can improve that.
+
+import re, sys
+import subprocess, shlex
 import time
 
-PLAY_CMD = "omxplayer -o hdmi ../Music/1-13-iwatodai-station.mp3"
-NMAP_CMD = "sudo nmap -sP -n 10.0.0.0/24"
 
-WYATT_ANDROID_MAC_ADDR = "ec:1f:72:1a:6a:b5"
-ANTONY_ANDROID_MAC_ADDR = "94:65:2d:24:6c:53"
+def parseFile( filename ):
+   # If the file gets abnormally large, then this is pretty gross
+   # because we hold the entire file in memory
+   lines = [ line.strip( '\n' ) for line in open( filename ) ]
+   if( len( lines ) % 2 == 1 ):
+      print "Improper macs.txt format"
+      sys.exit( 1 ) 
 
-MAC_ADDRS = []
-MAC_ADDRS.append(WYATT_ANDROID_MAC_ADDR)
-MAC_ADDRS.append(ANTONY_ANDROID_MAC_ADDR)
+   # convert the list into a dictionary such that even indexes are keys
+   # and odd indexes are values
+   macMap = {}
+   macMap = dict( zip( lines[::2], lines[1::2] ) )
+   return macMap 
 
-# statuses
-CONNECTED = "connected"
-NOT_FOUND = "not found"
+class DeviceStatus( object ):
+   # static Class variables
+   connected = "connected"
+   notFound = "not found"
+  
+   def __init__( self, filepath ):
+      self.status = DeviceStatus.notFound
+      self.timeAway = 60 * 10 + 1  # TODO: this is arbitrary
+      self.filepath = filepath
 
-# a map from devices to status
-STATUS = {}
-STATUS[WYATT_ANDROID_MAC_ADDR] = NOT_FOUND
-STATUS[ANTONY_ANDROID_MAC_ADDR] = NOT_FOUND
 
-TIME_AWAY = {}
-TIME_AWAY[WYATT_ANDROID_MAC_ADDR] = 15*60
-TIME_AWAY[ANTONY_ANDROID_MAC_ADDR] = 15*60
+#------initial setup should be in a class or some shit---------#
+# state variables
+connected_ = "connected"
+notFound_ = "not found"
+
+# parse the file to get a mapping of MAC addresses to filepaths of songs
+macAddrFile = 'macs.txt'
+macAddrMap = parseFile( macAddrFile )
+
+playCmd = "/usr/bin/omxplayer --no-keys -o hdmi"
+arpCmd = "sudo arp-scan -l"
+
+macStatus = {}
+for mac, filepath in macAddrMap.iteritems():
+   macStatus[ mac ] = DeviceStatus( filepath )
+#--------------------------------------------------------------#
+
+
+def log( msg ):
+   f = open( "/var/log/themesong.log", "a+" )
+   f.write( msg )
+   f.close()
 
 def detect():
-   playing = False
-
    while True:
-      p = subprocess.Popen( "sudo arp-scan -l", 
-                            stdout=subprocess.PIPE,
-                            shell=True )
-      (arp_output, err) = p.communicate()
-      p_status = p.wait()
+      p = subprocess.Popen( shlex.split( arpCmd ), 
+                            stdout=subprocess.PIPE )
+      ( arpOutput, err ) = p.communicate()
+      pStatus = p.wait()
 
       # search arp traffic for the MAC addr of each device
-      for mac in MAC_ADDRS:
-         mac_regex = re.compile( mac )
-         match = mac_regex.findall( arp_output )
+      for mac, _ in macStatus.iteritems():
+         log( "iterating...mac" )
+         macRegex = re.compile( mac )
+         match = macRegex.findall( arpOutput )
+           
+         devStatus = macStatus[ mac ]
 
-         prev_status = STATUS[mac]
          # if device was previously connected and it is no longer
          # present in arp traffic, forget the device
-         if prev_status == CONNECTED:
-            if len(match) == 0:
-               # register the device as disconnected
-               STATUS[mac] = NOT_FOUND
-               TIME_AWAY[mac] = 3
+         if( devStatus.status == DeviceStatus.connected ):
+            log( "Device is connected: " + mac + ", prev status: " + devStatus.status )
+
+            if( len( match ) ):
+               # MAC addr is appearing on the network, set timeAway to now
+               devStatus.timeAway = 0
             else:
-               TIME_AWAY[mac] = 0
+               # No match, register the device as disconnected
+               devStatus.status = DeviceStatus.notFound
+               devStatus.timeAway = 3
 
          # If we find a device that was previously not found,
-         # play their themesong!
-         if prev_status == NOT_FOUND:
+         # play its themesong!
+         elif( devStatus.status == DeviceStatus.notFound ):
+            log( "non connected: " + mac )
             if match:
                # register device as connected
-               STATUS[mac] = CONNECTED
-               if TIME_AWAY[mac] >= 15*60:
-                  player = subprocess.Popen( PLAY_CMD, 
-                                          stdout=subprocess.PIPE,
-                                          stdin=subprocess.PIPE, 
-                                          shell=True)
-                  playing = True
-                  time.sleep(3*60)
-                  playing = False
+               macStatus[ mac ].status = DeviceStatus.connected
+
+               if( devStatus.timeAway >= 60 * 10 ):
+                  playSongCmd = playCmd + ' ' + devStatus.filepath
+                  player = subprocess.Popen( shlex.split( playSongCmd ),
+                                             stdout=subprocess.PIPE,
+                                             stdin=subprocess.PIPE )
+                  # TODO: sleep until the song is over...which could be more than 3min
+                  time.sleep( 3 * 60 )
                   
-               TIME_AWAY[mac] = 0
+               devStatus.timeAway = 0
+               log( "Finished waiting for song to end: " mac )
+
             else:
-               TIME_AWAY[mac] = TIME_AWAY[mac] + 3
+               log( "Mac addr not present and no match on recent arpScan: " + mac )
+               devStatus.timeAway += 3
+
+         else:
+            log( "Unrecognized device status" )
+            sys.exit( 1 )
 
       # sleep for a brief moment before scanning arp traffic again
       time.sleep(3) 
+
 
 if __name__ == '__main__':
    detect()
